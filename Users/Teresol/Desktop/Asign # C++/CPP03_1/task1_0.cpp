e4febdd45e50aa3a6a6f996d666f10d3d2d5fa78
UmairSajid-1 <umii7861122@gmail.com>
2024-07-27 10:21:46 -0700

    CPP02_1

---
+++ Users/Teresol/Desktop/Asign # C++/CPP03_1/task1_0.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+*                                                                        *
+*          Name: Umair Sajid                                             *
+*          Date: 27-07-2024                                              *
+*          Description:   program to demonstrate the scope and lifetime  *
+*         of local, global, and static variables.                        *
+*                                                                        *
+**************************************************************************
+*   I certify that I have neither given nor received unauthorized aid    *
+*   on this Assignment.                                                  *
+*************************************************************************/
+#include <iostream>
+using namespace std;   
+int main() 
+{
+    cout << " Hello World "<< endl;
+    return 0;
+}
